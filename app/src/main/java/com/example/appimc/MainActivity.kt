3e7ea996b515ba7d1783bb9a86c49dacf3c0838d
Umo Balde <balde.1999.umo@gmail.com>
2025-06-11 15:41:51 +0100

    Exibir um texto na tela

--- app/src/main/java/com/example/appimc/MainActivity.kt
+++ app/src/main/java/com/example/appimc/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.appimc.ui.theme.AppIMCTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +28,20 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+//Criar a interface IMCScreen
 @Composable
 fun IMCScreen() {
+    //Organiza os elementos verticalmemte
+    Column (
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ){
+        //Exibe um texto na tela
+        Text(text = "Calculadora de IMC", style = MaterialTheme
+            .typography.headlineMedium)
+
+    }
 
 }
 
