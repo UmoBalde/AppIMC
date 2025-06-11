219e0f5dc2cfa84a232349933cc06601935778d5
Umo Balde <balde.1999.umo@gmail.com>
2025-06-11 16:05:24 +0100

    Imagem do fundo

--- app/src/main/java/com/example/appimc/MainActivity.kt
+++ app/src/main/java/com/example/appimc/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,7 +14,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appimc.ui.theme.AppIMCTheme
@@ -31,10 +36,24 @@ class MainActivity : ComponentActivity() {
 //Criar a interface IMCScreen
 @Composable
 fun IMCScreen() {
+
+    Box (
+        modifier = Modifier.fillMaxSize()
+    ){
+        //Imagem de fundo
+        Image(
+            painter = painterResource(id = R.drawable.fundo),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+
+        )
+    }
     //Organiza os elementos verticalmemte
     Column (
         modifier = Modifier.fillMaxSize()
             .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
         //Exibe um texto na tela
